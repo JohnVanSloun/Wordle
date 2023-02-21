bc8f05dd37e86e7c7bdfec4030bd9c80abc38465
John Van Sloun <jpvansloun@gmail.com>
2023-02-21 14:28:34 -0600

    added tesing file

--- tester.py
+++ tester.py
@@ -1,18 +1,26 @@
 import wordle
 
 # find_repeats tests
-#
-# print(wordle.find_repeats("check"))
-# print(wordle.find_repeats("teeth"))
-# print(wordle.find_repeats("books"))
-# print(wordle.find_repeats("gamer"))
-#
+
+print(wordle.find_num_repeats("check"))
+print(wordle.find_num_repeats("teeth"))
+print(wordle.find_num_repeats("books"))
+print(wordle.find_num_repeats("gamer"))
+
 # has_repeats tests
-#
-# print(wordle.has_repeats("check"))
-# print(wordle.has_repeats("teeth"))
-# print(wordle.has_repeats("books"))
-# print(wordle.has_repeats("gamer"))
+
+print(wordle.has_repeats("check"))
+print(wordle.has_repeats("teeth"))
+print(wordle.has_repeats("books"))
+print(wordle.has_repeats("gamer"))
+
+# no_repeats_yellow_and_grey tests
+
+print(wordle.no_repeats_yellow_and_grey("stork", "steam", 2))
+
+# excess_repeats_yellow_and_grey tests
+
+print(wordle.excess_repeats_yellow_and_grey("cooks", "cooko", 2))
 
 # check_word tests
 
@@ -21,3 +29,6 @@ print(wordle.check_word("methe", "teeth"))
 print(wordle.check_word("chair", "books"))
 print(wordle.check_word("chair", "gamer"))
 print(wordle.check_word("cohoe", "cooks"))
+print(wordle.check_word("books", "cooko"))
+print(wordle.check_word("books", "tasch"))
+print(wordle.check_word("books", "ooooo"))
